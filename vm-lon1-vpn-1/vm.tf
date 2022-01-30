39a345cfeb5e6411c08b31fac6b94bcbc8600145
vattenbit <vattenbit@gmail.com>
2022-01-30 19:43:18 +0000

    Add ssh key, test connection and remote-exec example

--- vm-lon1-vpn-1/vm.tf
+++ vm-lon1-vpn-1/vm.tf
@@ -3,6 +3,25 @@ resource "digitalocean_droplet" "vm-lon1-vpn-1" {
   size   = "s-1vcpu-1gb"
   image  = "ubuntu-20-04-x64"
   region = "lon1"
+  ssh_keys = [
+      data.digitalocean_ssh_key.terraform.id
+    ]
+
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install nginx
+      "sudo apt update"
+    ]
+  }
 }
 
 resource "digitalocean_project" "prj-01" {
